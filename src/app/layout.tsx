67458fb59b6781d89d296bb0fc3c78abea6d24d6
padaa880 <prashantchaudhari881@gmail.com>
2024-05-09 23:58:06 +0530

    add hero section and pricing section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,22 +7,18 @@ import { ThemeProvider } from "@/providers/theme-provider";
 const font = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Fuzzie",
-  description: "Automate Your Work With Fuzzie.",
+	title: "Fuzzie",
+	description: "Automate Your Work With Fuzzie.",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={font.className}>
-        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
-          {children}
-        </ThemeProvider>
-      </body>
-    </html>
-  );
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+	return (
+		<html lang="en">
+			<body className={font.className}>
+				<ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
+					{children}
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
